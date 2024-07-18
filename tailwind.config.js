99ea972d7bad51fd65206c55d88a4e492db1c7e2
RezaEbrahimi81 <r3z4.3br4h1m1@gmail.com>
2024-07-18 21:51:50 +0330

    END GRID WIDGET ELEMENT

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,41 @@
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      padding:{
+        DEFAULT: '15PX',
+
+      }
+
+    },
+    screens:{
+      sm: '640px',
+      md: '768px',
+      lg: '960px',
+      xl: '1280px',
+    },
+    fontFamily:{
+      primary: 'khand',
+      secondary: 'sans-serif'
+    },
+    backgroundImage:{
+      bg: 'url(/assets/)',
+    },
+
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '##ccc',
+          hover: '#343e4a'
+        },
+        secondary: '#8E7861',
+        accent: {
+          DEFAULT: '#cda274',
+          secondary: '#f4f0ec',
+          hover: '#b88c5d',
+        },
+      },
+    },
   },
   plugins: [],
 }
